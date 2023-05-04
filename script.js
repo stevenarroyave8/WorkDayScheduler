9ed8c981cb5a588687ce7ecc24b526df805f7828
stevenarroyave8 <stevenarroyave8@gmail.com>
2023-05-03 23:09:58 -0400

    adding function for past, present, future color change

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
+const localeSettings = {};
+dayjs.locale(localeSettings);
+// Wait until the DOM is fully loaded before executing the code inside the function.
+$(function () {
+  // Get the current hour of the day using the dayjs library.
+  const currentHour = dayjs().format('H');
+// The function below changes the color of each time block based on whether it's in the "past, present, or future" relative to the current hour.
+  function hourlyColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      $(this).toggleClass('past', blockHour < currentHour);
+      $(this).toggleClass('present', blockHour === currentHour);
+      $(this).toggleClass('future', blockHour > currentHour);
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +45,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
