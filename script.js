2930342f74daae1b3a7b46ac75afabc87fd083a5
Steven Arroyave <sarroyave@legendfunding.com>
2023-05-04 12:58:53 -0400

    added function to refresh time every second

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ $(function () {
   // Get the current hour of the day 
   const currentHour = dayjs().format('H');
 // The function below changes the color of each time block based on whether it's in the "past, present, or future" relative to the current hour.
-  function hourlyColor() {
-    $('.time-block').each(function() {
-      const blockHour = parseInt(this.id);
-      $(this).toggleClass('past', blockHour < currentHour);
-      $(this).toggleClass('present', blockHour === currentHour);
-      $(this).toggleClass('future', blockHour > currentHour);
-    });
-  }
+function hourlyColor() {
+  $('.time-block').each(function() {
+    const blockHour = parseInt(this.id);
+    $(this).toggleClass('past', blockHour < currentHour);
+    $(this).toggleClass('present', blockHour === currentHour);
+    $(this).toggleClass('future', blockHour > currentHour);
+  });
+}
 
   // Function to save the users input to localStorage
   function textEntry() {
@@ -44,6 +44,23 @@ $(function () {
     $(this).children('.description').val(value);
   });
 
+  // Function to refresh time every second 
+  function updateTime() {
+    const dateElement = $('#date');
+    const timeElement = $('#time');
+    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    const currentTime = dayjs().format('hh:mm:ss A');
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  }
+
+  hourlyColor();
+  textEntry();                
+  refreshColor();
+
+ 
+  setInterval(updateTime, 1000);
+});
 
 
 
